a21f0e22ec36f1ec0caa5f1b6c7105cd8f2a30ce
padams73 <pad@stac.school.nz>
2022-02-25 13:17:26 +1300

    Deleting removes image too
    Now when we delete a student it also removes their photo, using unlink()

--- StudentDB/deletestudent.php
+++ StudentDB/deletestudent.php
@@ -6,12 +6,28 @@
     // If not, redirect back to deletestudentselect.php
     header("Location: index.php?page=deletestudentselect&error=noselect");
   } else {
-    // Delete selected student
+    // Set variable to store studentID
     $studentID = $_GET['studentID'];
+
+    // Get the name of the photo file for this student so we can delete it too
+    $photo_sql = "SELECT photo from student WHERE studentID=$studentID";
+    $photo_qry = mysqli_query($dbconnect, $photo_sql);
+    $photo_aa = mysqli_fetch_assoc($photo_qry);
+    $photo = $photo_aa['photo'];
+    // Delete the photo using the unlink() function
+    // Set file path first
+    $file_path = "images/".$photo;
+    echo $file_path;
+      unlink("$file_path");
+
+
+
+    // Delete the student
     $sql = "DELETE FROM student WHERE studentID = $studentID";
     $qry = mysqli_query($dbconnect, $sql);
     echo "<h2>Student deleted</h2>";
   }
 
  ?>
+ <p>Student successfully deleted</p>
  <a href="index.php">Back to home page</a>
